7157ee19623f041354b5c3c9e967885ae686c380
Heersin <teablearcher@gmail.com>
2021-01-13 10:06:48 +0800

    update user model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose'
 import validator from 'validator'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema({
 
@@ -63,4 +65,55 @@ const userSchema = new mongoose.Schema({
     }
 },
     { timestamps: true, },
+    { toJSON: 
+        { virtuals: true }, },
+    { toObject:
+        { virtuals: true }}
 )
+
+// userSchema.set('toObject', {virtuals: true})
+// userSchema.set('toJSON', {virtuals: true})
+
+// middle ware pre
+// save
+userSchema.pre('save', async function(next){
+    // hash password
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
+
+
+// define instance methods
+userSchema.methods.generateAuthToken = async function() {
+    // token generation
+    const user = this
+    const token = jwt.sign({_id: user._id}, process.env.JWT_KEY);
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
+
+// static functions
+userSchema.statics.findByEmailAndPwd = async (email, password) => {
+    function findCredentialError(error){
+        this.error = error
+        console.log("cannot find by email and password")
+    }
+
+    findCredentialError.prototype = new Error()
+
+    const user = await user.findOne({email})
+    if (!user){throw new findCredentialError("No such mail")}
+
+    const isPwd = await bcrypt.compare(password, user.password)
+    if (!isPwd){throw new findCredentialError("password not matched")}
+
+    return user
+}
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
